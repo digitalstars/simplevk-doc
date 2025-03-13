d4325886864c3501d78243ed31314724e6d22bc5
DigitalStars <46852707+digitalstars@users.noreply.github.com>
2025-03-14 01:41:15 +0300

    Delete docs/.vuepress/.temp directory

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "C:/Users/mrkir/PhpstormProjects/simplevk-doc/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_2dbfef188ccfd5198302272d85f9af6d/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "C:/Users/mrkir/PhpstormProjects/simplevk-doc/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_2dbfef188ccfd5198302272d85f9af6d/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
